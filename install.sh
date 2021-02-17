299812dd2c7dfd98c26b291824fd75e6765c3bca
Jacob Scrapchansky <71794171+Furrure@users.noreply.github.com>
2021-02-17 10:46:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo "Installing discord API"
 python3 -m pip install discord.py
 echo "Moving to executable Directory"
-cp $PWD/dca.py /usr/local/bin
+cp $PWD/dca.py /usr/local/bin/dca
+chmod a+x /usr/local/bin/dca
